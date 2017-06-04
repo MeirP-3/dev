58d8888b8788275c59fef5935850640cdf3e7e3c
Meir <pechtalt@gmail.com>
2017-06-04 17:22:12 +0300

    improvements to gallery

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-// function startPreview() {
-//     document.querySelector('.container').classList.remove('hidden');
-//     document.querySelector('.welcome-container').classList.add('hidden');
-// }
-
 $(window).on('load', function() {
     $('.welcome-container').removeClass('hidden');
     new Audio('sound/right.mp3').play();
